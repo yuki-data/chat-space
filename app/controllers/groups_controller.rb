19fc759795454fd5901ef8f71a4ff5cd7b444980
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-10-30 12:40:01 +0900

    update group#create: create group data from form input

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,9 +3,13 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
   def create
+    Group.create(group_params)
   end
   def edit
   end
   def update
+  end
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
