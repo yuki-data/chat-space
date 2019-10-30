b374ea848066f054743cb52ccd2d09555a06f33a
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-10-30 15:25:04 +0900

    add empty lined for code clarity

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,7 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
   def create
     group = Group.create(group_params)
     user_ids = params.require(:group)[:user_ids]
@@ -9,10 +10,13 @@ class GroupsController < ApplicationController
       GroupUser.create(group_id: group.id, user_id: i.to_i) unless i.empty?
     end
   end
+
   def edit
   end
+
   def update
   end
+
   def group_params
     params.require(:group).permit(:name)
   end
