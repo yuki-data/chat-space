80109b454bee48ea39529bb4a33dabec002bb26a
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-10-31 17:25:10 +0900

    messages table include users tables to suppress addtional queries

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,6 +20,7 @@ class MessagesController < ApplicationController
   def set_message
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
     @message = Message.new
   end
 end
