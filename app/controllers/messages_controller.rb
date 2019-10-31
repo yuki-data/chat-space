43c2f08dc53f496e813c27ea5f76dc680e133038
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-10-31 13:30:22 +0900

    set @groups, @group, @message in messages#create since the variables is necessary to render index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,7 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:index, :create]
+
   def index
-    @groups = current_user.groups
-    @group = Group.find(params[:group_id])
-    @message = Message.new
   end
 
   def create
@@ -12,5 +11,11 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
+  def set_message
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 end
