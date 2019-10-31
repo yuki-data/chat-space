8c4f2604db00010d2891a205ae3026ffd9cfbd9a
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-10-31 12:43:19 +0900

    replace form tag with form_for in messages#index view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
   def create
