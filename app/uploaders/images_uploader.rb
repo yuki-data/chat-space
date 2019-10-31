177ba3026078a96f827f8d37116f832d791d6d45
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-10-31 17:13:23 +0900

    use MiniMagick to resize uploaded image

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -6,6 +6,8 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [200, 200]
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
