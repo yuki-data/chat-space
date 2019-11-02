dcdfd49ce3efdddfec45789895e213ad549db4a4
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-02 20:52:42 +0900

    refine rspec to clarify which instance variable is subject to test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -17,22 +17,40 @@ describe MessagesController do
         login user
       end
 
-      it "アクション内で定義しているインスタンス変数があるか" do
-        groups = create_list(:group, 3)
-        groups.each do |g|
-          g.users << user
-        end
-        messages = create_list(:message, 3, group: group)
-        get :index, params: {group_id: group.id}
-        expect(assigns(:groups)).to match groups
-        expect(assigns(:group)).to eq group
-        expect(assigns(:messages)).to match messages
-        expect(assigns(:message)).to be_a_new Message
-      end
-
       it "該当するビューが描画されているか" do
         get :index, params: {group_id: group.id}
         expect(response).to render_template :index
+      end
+
+      describe "アクション内で定義しているインスタンス変数があるか" do
+        let(:groups) do
+          groups = create_list(:group, 3)
+          groups.each do |g|
+            g.users << user
+          end
+          groups
+        end
+        let(:messages) { create_list(:message, 3, group: group) }
+
+        before do
+          get :index, params: {group_id: group.id}
+        end
+
+        it "@groupsがあるか" do
+          expect(assigns(:groups)).to match groups
+        end
+
+        it "@groupがあるか" do
+          expect(assigns(:group)).to match group
+        end
+
+        it "@messagesがあるか" do
+          expect(assigns(:messages)).to match messages
+        end
+
+        it "@messageがあるか" do
+          expect(assigns(:message)).to be_a_new Message
+        end
       end
     end
   end
