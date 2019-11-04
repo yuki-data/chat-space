a397f794a20a14f6b2914d7b81cda539c8b35f38
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-04 21:34:13 +0900

    refine: remove unnecessary jquery code

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -21,7 +21,7 @@ $(function() {
   $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr("action");
+    var url = this.action;
     $.ajax({
       url: url,
       type: "POST",
