2508d7811a13329eace1feb827e54ce1db47b79b
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-04 21:12:53 +0900

    set submit event listener to form

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,23 @@
+var $;
+$(function() {
+  $("#new_message").on("submit", function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+      .done(function(data) {
+        console.log(data);
+      })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+      });
+    return false;
+  });
+});
