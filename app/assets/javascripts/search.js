b875d3085a07b7101772934fb71c8f07cfadc32f
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-05 20:42:55 +0900

    add data-attributes to group member template to improve data handling

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -27,7 +27,7 @@ $(document).on("turbolinks:load", function() {
     <div class="chat-group-user js-chat-member" id="chat-group-user-${userId}" >
       <input name="group[user_ids][]" type="hidden" value="${userId}" />
       <p class="chat-group-user__name">${userName}</p>
-      <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" >削除</a >
+      <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${userId}" data-user-name="${userName}" >削除</a >
     </div>
     `;
     chatMemberList.append(html);
