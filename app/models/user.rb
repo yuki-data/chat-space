be9ba2d7adad50174e63348cae09f3f9e9dca883
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-05 17:32:09 +0900

    add search method to User model to query for keyword matches

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :messages
+
+  def self.search(keyword)
+    return nil if keyword.strip.empty?
+    User.where('name LIKE(?)', "%#{keyword}%")
+  end
 end
