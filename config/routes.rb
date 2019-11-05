989aa8b29f02bcde68b7cac7f2aeb74e36dd6517
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-05 16:41:00 +0900

    add route to users#index to respond to requeset for group member search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
-  resources :users, only: ["edit", "update"]
+  resources :users, only: ["index", "edit", "update"]
   resources :groups, only: ["edit", "update", "new", "create"] do
     resources :messages, only: ["index", "create"]
   end
