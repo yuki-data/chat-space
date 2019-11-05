33effc9adf753b339cec84458351824f758f7a74
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-05 17:54:23 +0900

    embed user search results to html

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,26 @@
 var $;
 $(document).on("turbolinks:load", function() {
+  var searchResult = $("#user-search-result");
+
+  function appendUser(user) {
+    var html = `
+    <div class="chat-group-user">
+      <p class="chat-group-user__name">${user.user_name}</p>
+      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.user_name}" >追加</a>
+    </div>
+    `;
+    searchResult.append(html);
+  }
+
+  function appendErrMsgToHTML(text) {
+    var html = `
+    <div class="chat-group-user">
+      <p class="chat-group-user__name">${text}</p>
+    </div>
+    `;
+    searchResult.append(html);
+  }
+
   $("#user-search-field").on("input", function() {
     var input = this.value;
     console.log(input);
@@ -11,6 +32,15 @@ $(document).on("turbolinks:load", function() {
     })
       .done(function(data) {
         console.log(data);
+        $("#user-search-result").empty();
+        if (data.length !== 0) {
+          data.forEach(function(user) {
+            appendUser(user);
+          });
+        } else {
+          appendErrMsgToHTML("一致するユーザーがいません");
+          console.log("一致なし");
+        }
       })
       .fail(function() {
         alert("error");
