c79301465eca40c321f22df809250bd05aeee3f1
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-05 20:20:59 +0900

    add jquery click event linstener on dynamically created element

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -46,4 +46,8 @@ $(document).on("turbolinks:load", function() {
         alert("ユーザー検索に失敗しました");
       });
   });
+
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log(this);
+  });
 });
