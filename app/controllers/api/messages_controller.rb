4e1c8943890052b4658feaa1e3ce8ec00202df52
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-06 17:48:29 +0900

    rename id to latest_message_id for clarity

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,7 @@
 class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
-    latest_message_id = params[:id].to_i
+    latest_message_id = params[:latest_message_id].to_i
     @messages = group.messages.includes(:user).where("id > ?", latest_message_id)
   end
 end
