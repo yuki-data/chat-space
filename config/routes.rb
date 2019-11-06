ddffb8f96f933947ddbf3c5438fcec2fa0622471
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-06 16:16:59 +0900

    fix routing: nest api::messages under group to pass group_id to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
-  namespace :api do
-    resources :messages, only: ["index"]
-  end
   resources :users, only: ["index", "edit", "update"]
   resources :groups, only: ["edit", "update", "new", "create"] do
     resources :messages, only: ["index", "create"]
+    namespace :api do
+      resources :messages, only: ["index"]
+    end
   end
 end
