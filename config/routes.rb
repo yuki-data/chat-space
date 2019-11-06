fbf808c6e91209375fcbe86dcce2f28e61fb5769
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-06 16:00:03 +0900

    set route to api::messages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
+  namespace :api do
+    resources :messages, only: ["index"]
+  end
   resources :users, only: ["index", "edit", "update"]
   resources :groups, only: ["edit", "update", "new", "create"] do
     resources :messages, only: ["index", "create"]
