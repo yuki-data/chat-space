c11aef4dad9b8c1aeb13d16d6a2bb9c81e8ce702
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-08 14:53:06 +0900

    update capistrano config to load AWS keys from environ vars when deployed

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,13 @@ namespace :deploy do
   end
 end
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
