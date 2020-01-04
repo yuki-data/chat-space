9a7d1aea31c36817abd7e23148d8ae45102d26ff
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-04 15:32:27 +0900

    carrierwave config for s3 image upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'yuki-chatspace-image'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yuki-chatspace-image'
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'yukidata-chatspace'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yukidata-chatspace'
+  end
 end
