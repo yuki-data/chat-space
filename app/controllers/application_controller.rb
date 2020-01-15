0a197db438cf452065d7e68095c8f909c3bbc451
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-15 19:43:37 +0900

    set basic auth for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic_auth, if: :production?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -7,5 +8,17 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      valid_user = username == "admin"
+      valid_password = password == Rails.application.secrets.basic_auth_password
+      valid_user && valid_password
+    end
   end
 end
